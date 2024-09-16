7e4526e877eaa8e3fd021430a04739d7feac5a5e
Carlos <juancaclavijo2005@gmail.com>
2024-09-16 06:55:20 -0500

    Solucionado bug carga infinita y correcciones ortograficas

--- Backend/Controllers/Productos/getProducts.php
+++ Backend/Controllers/Productos/getProducts.php
@@ -9,7 +9,9 @@
                 echo json_encode($productController);
             }
             else{
-                echo "Error";
+                $objetoError = new stdClass;
+                $objetoError->Error = "Ha ocurrido un error obteniendo los productos";
+                echo json_encode($objetoError);
             }
         }
     }
